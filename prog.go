5149691ec074c58c5e3ed06521950b0cd6fd0516
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2024-09-19 01:30:01 -0400

    flags

--- prog.go
+++ prog.go
@@ -2,10 +2,13 @@ package jqx
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"io/fs"
 	"iter"
+	"maps"
+	"runtime/debug"
 	"slices"
 )
 
@@ -36,18 +39,49 @@ type Program struct {
 	StdinIsTerminal  bool
 	StdoutIsTerminal bool
 }
+type flags struct {
+	script    string
+	filenames []string
+
+	raw bool
+	dry bool
+}
+
+func (f *flags) populate(args []string) {
+	fset := flag.NewFlagSet("", flag.ExitOnError)
+	fset.BoolVar(&f.raw, "r", false, `stdin, stdout, and files are newline-separated strings (unimplemented)`)
+	fset.BoolVar(&f.dry, "dry-run", false, `don't persist snapshots`)
+
+	usage := fset.Usage
+	fset.Usage = func() {
+		usage()
+		out := fset.Output()
+
+		info, _ := debug.ReadBuildInfo()
+		if info != nil {
+			fmt.Fprintln(out)
+			fmt.Fprintln(out, "version", info.Main.Version, "built with", info.GoVersion)
+			fmt.Fprintf(out, "Update: go install %s@latest\n\n", info.Path)
+		}
+	}
+
+	fset.Parse(args)
+
+	args = fset.Args()
+	f.script = "."
+	if len(args) > 0 {
+		f.script, f.filenames = args[0], args[1:]
+	}
+}
 
 func (p *Program) Main() (rtErr error) {
 	defer catch[failError](&rtErr)
 
-	script := "."
-	filenames := p.Args
-	if len(filenames) > 0 {
-		script, filenames = filenames[0], filenames[1:]
-	}
+	var f flags
+	f.populate(p.Args)
 
 	files := map[string]any{}
-	for _, filename := range filenames {
+	for _, filename := range f.filenames {
 		file, err := p.Open(filename)
 		failif(err, "loading")
 		defer file.Close()
@@ -75,12 +109,19 @@ func (p *Program) Main() (rtErr error) {
 		}
 	}
 
-	query := p.State.Compile(constString(script))
+	query := p.State.Compile(constString(f.script))
 	for v := range input {
 		for v := range query(v) {
 			v := must(marshal(v))
 			fmt.Fprintln(p.Stdout, string(v))
 		}
+	}
+
+	if f.dry {
+		for _, file := range slices.Collect(maps.Keys(p.State.Files)) {
+			fmt.Fprintln(p.Stdout, file)
+		}
+		p.State.Files = nil
 	}
 
 	return nil

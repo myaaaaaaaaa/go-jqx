e3a408fb33c05b7fb0e5d0fc0d25430f2b640317
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-10-21 19:25:16 -0400

    clean up TestHtmlExtractText

--- html_test.go
+++ html_test.go
@@ -33,38 +33,37 @@ func TestHTMLQuerySelector(t *testing.T) {
 }
 
 func TestHtmlExtractText(t *testing.T) {
-	// Test: Simple HTML
-	html1 := `<html><body><p>Hello, world!</p></body></html>`
-	expected1 := "Hello, world!"
-	assertEqual(t, htmlExtractText(html1), expected1)
+	assert := func(html, want string) {
+		t.Helper()
+		assertEqual(t, htmlExtractText(html), want)
+	}
 
-	// Test: HTML with multiple text nodes
-	html2 := `<h1>Title</h1><p>First paragraph.</p><p>Second paragraph.</p>`
-	expected2 := "TitleFirst paragraph.Second paragraph."
-	assertEqual(t, htmlExtractText(html2), expected2)
-
-	// Test: HTML with no text
-	html3 := `<div><img src="image.jpg"/></div>`
-	expected3 := ""
-	assertEqual(t, htmlExtractText(html3), expected3)
-
-	// Test: Empty string
-	html4 := ""
-	expected4 := ""
-	assertEqual(t, htmlExtractText(html4), expected4)
-
-	// Test: HTML with comments
-	html5 := `<!-- This is a comment --><body><p>Some text</p></body>`
-	expected5 := "Some text"
-	assertEqual(t, htmlExtractText(html5), expected5)
-
-	// Test: Plain text
-	html6 := "Hello"
-	expected6 := "Hello"
-	assertEqual(t, htmlExtractText(html6), expected6)
-
-	// Test: Alt text
-	html7 := `<p>Hello</p><img src="image.jpg" alt="olleh"/>`
-	expected7 := "Hello\n(img: olleh)\n"
-	assertEqual(t, htmlExtractText(html7), expected7)
+	assert(
+		`<html><body><p>Hello, world!</p></body></html>`,
+		"Hello, world!",
+	)
+	assert(
+		`<h1>Title</h1><p>First paragraph.</p><p>Second paragraph.</p>`,
+		"TitleFirst paragraph.Second paragraph.",
+	)
+	assert(
+		`<div><img src="image.jpg"/></div>`,
+		"",
+	)
+	assert(
+		"",
+		"",
+	)
+	assert(
+		`<!-- This is a comment --><body><p>Some text</p></body>`,
+		"Some text",
+	)
+	assert(
+		"Hello",
+		"Hello",
+	)
+	assert(
+		`<p>Hello</p><img src="image.jpg" alt="olleh"/>`,
+		"Hello\n(img: olleh)\n",
+	)
 }

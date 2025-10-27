87cc9c9dcbc732fa69bbbb8981f1b00d57ca9651
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-10-27 01:24:27 -0400

    improve proptest tests

--- proptest/random_test.go
+++ proptest/random_test.go
@@ -9,30 +9,34 @@ import (
 func TestChars(t *testing.T) {
 	r := Rand(10)
 	for range 200 {
-		got := r.Chars("aaaaaa")
-		if strings.Trim(got, "a") != "" {
-			t.Error(got, "contains non-a")
+		for i := range 5 {
+			got := r.Chars(strings.Repeat("a", i+1))
+			if strings.Trim(got, "a") != "" {
+				t.Error(got, "contains non-a")
+			}
 		}
 
-		got = r.Chars("abcde")
+		got := r.Chars("abcde")
 		if strings.Trim(got, "abcde") != "" {
 			t.Error(got, "contains non-abcde")
 		}
 	}
 }
 func TestCharsNotIn(t *testing.T) {
-	r := Rand(10)
-	for range 5 {
-		got := r.Chars("abc")
+	const charSet = "abcdefg"
 
-		s := strings.Split(got, "")
-		slices.Sort(s)
-		s = slices.Compact(s)
-		if strings.Join(s, "") == "abc" {
-			return
-		}
+	r := Rand(10)
+	got := ""
+	for range 20 {
+		got += r.Chars(charSet)
 	}
-	t.Error("statistical anomaly")
+
+	s := strings.Split(got, "")
+	slices.Sort(s)
+	s = slices.Compact(s)
+	if strings.Join(s, "") != charSet {
+		t.Error(got)
+	}
 }
 
 func TestCharsActuallyRandom(t *testing.T) {
@@ -45,5 +49,38 @@ func TestCharsActuallyRandom(t *testing.T) {
 		if doNotWant == got {
 			t.Error("not random enough")
 		}
+	}
+}
+
+func TestStrings(t *testing.T) {
+	r := Rand(15)
+	for range 200 {
+		got := r.Strings("   ", " hello ")
+		for s := range strings.FieldsSeq(got) {
+			if s != "hello" {
+				t.Error(s)
+			}
+		}
+
+		for i := range 5 {
+			got := r.Strings(make([]string, i+1)...)
+			if got != "" {
+				t.Error(got)
+			}
+		}
+	}
+}
+func TestStringsNotIn(t *testing.T) {
+	r := Rand(22)
+	got := ""
+	for range 10 {
+		got += r.Strings(" hello ", "    world!    ")
+	}
+
+	s := strings.Split(got, " ")
+	slices.Sort(s)
+	s = slices.Compact(s)
+	if strings.Join(s, "") != "helloworld!" {
+		t.Error(got)
 	}
 }

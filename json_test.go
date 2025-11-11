da8518648757706a2b6b07cdecd1ad2bee87b275
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-11-10 23:35:06 -0500

    slim down jsonTokenize() tests

--- json_test.go
+++ json_test.go
@@ -1,26 +1,17 @@
 package jqx
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestJsonTokenize(t *testing.T) {
 	jsonStr := `{"name": "John", "age": 30, "isStudent": false, "courses": ["Math", "Science"]}`
-	expected := []string{"name", "John", "age", "30", "isStudent", "false", "courses", "Math", "Science"}
-	actual, err := jsonTokenize(jsonStr)
-	if err != nil {
-		t.Errorf("jsonTokenize() error = %v", err)
-	}
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("jsonTokenize() = %v, want %v", actual, expected)
-	}
+	got := must(jsonTokenize(jsonStr))
+	assertString(t, got, `[name John age 30 isStudent false courses Math Science]`)
 }
 
 func TestJsonTokenize_MalformedJSON(t *testing.T) {
 	jsonStr := `{"name":,}`
 	_, err := jsonTokenize(jsonStr)
-	if err == nil {
-		t.Errorf("jsonTokenize() error = %v, wantErr %v", err, true)
-	}
+	assertEqual(t, err != nil, true)
 }
